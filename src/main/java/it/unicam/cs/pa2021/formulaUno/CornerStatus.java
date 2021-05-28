fdfc8dbd88238510b27bf1fcc52f1ef63ca1d388
Riccardo Pierini <riccardo.pierini@studenti.unicam.it>
2021-05-28 10:49:47 +0200

    Adding a new status in the CornerStatus enumeration.

--- src/main/java/it/unicam/cs/pa2021/formulaUno/CornerStatus.java
+++ src/main/java/it/unicam/cs/pa2021/formulaUno/CornerStatus.java
@@ -5,9 +5,11 @@ package it.unicam.cs.pa2021.formulaUno;
  * OUT_OF_RACE --> angolo fuori dalla pista nel campo di gioco.
  * IN_RACE --> angolo dentro la pista nel campo di gioco.
  * CROSSED --> angolo attraversato da una macchina da un certo giocatore in un certo tempo.
+ * GOAL --> angolo facente parte del traguardo.
  */
 public enum CornerStatus {
     OUT_OF_RACE,
     IN_RACE,
-    CROSSED;
+    CROSSED,
+    GOAL;
 }
