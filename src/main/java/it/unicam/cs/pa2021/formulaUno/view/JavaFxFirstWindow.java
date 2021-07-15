542053425177a5ff72c65358a26b2a624ca4ccd5
Riccardo Pierini <riccardo.pierini@studenti.unicam.it>
2021-07-15 17:00:31 +0200

    Problems with the compilator.

--- src/main/java/it/unicam/cs/pa2021/formulaUno/view/JavaFxFirstWindow.java
+++ src/main/java/it/unicam/cs/pa2021/formulaUno/view/JavaFxFirstWindow.java
@@ -6,8 +6,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,24 +14,16 @@ public class JavaFxFirstWindow implements Initializable {
     @FXML
     private ComboBox<String> comboBoxChoiceFields;
 
-    @FXML
-    private Button nextButton;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ObservableList<String> options = FXCollections.observableArrayList("circuito piccolo (3 giocatori)", "circuito medio (4 giocatori");
         comboBoxChoiceFields = new ComboBox<>(options);
     }
 
-    @FXML
-    void clickContinue(ActionEvent event) {
+    public void clickContinue(javafx.event.ActionEvent actionEvent) {
         if(!comboBoxChoiceFields.getValue().equals("")){
             if(comboBoxChoiceFields.getValue().equals("circuito piccolo (3 giocatori)"))
                 System.out.println("a");
         }
-    }
-
-    @FXML
-    public void clickComboBox(ActionEvent actionEvent) {
     }
 }
